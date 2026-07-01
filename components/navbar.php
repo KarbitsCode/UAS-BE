830b4643addad75ef24c3a2aacb00a1165c794b9
evayanti00 <evayantimade9@gmail.com>
2026-07-01 16:51:20 +0800

    membuat hlm dashboard, sidebar, navbar

---
+++ components/navbar.php
@@ -0,0 +1,28 @@
+<nav class="topbar">
+
+    <div class="d-flex align-items-center">
+
+        <button class="toggle-btn" id="toggleSidebar">
+            ☰
+        </button>
+
+        <input
+            type="text"
+            class="search"
+            placeholder="Cari event...">
+
+    </div>
+
+    <div class="profile">
+
+        <button class="btn btn-primary">
+            + Buat Event
+        </button>
+
+        <div class="org">
+            HMTI
+        </div>
+
+    </div>
+
+</nav>
